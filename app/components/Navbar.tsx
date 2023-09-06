6b493e0ca0f1182fa30f4aeaab7a807041a43be5
Michael Davis <jazzupchristian1997@gmail.com>
2023-09-06 19:14:39 -0400

    fixed navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
 import Image from "next/image";
-
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 interface navigationPieces {
   name: string;
@@ -15,7 +14,7 @@ interface navigationPieces {
 }
 
 let navigation: navigationPieces[] = [
-  { name: "Home", href: "/", current: true },
+  { name: "Home", href: "/", current: false },
   { name: "About", href: "/about", current: false },
   { name: "Encounters", href: "/encounters", current: false },
   { name: "Give", href: "/giving", current: false },
@@ -30,100 +29,148 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 bg-white drop-shadow z-40">
-      <Popover as="div" className=" z-40">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between px-6 pb-6 pt-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex flex-1 items-center justify-between">
-            <div className="flex w-full items-center justify-between lg:w-auto">
-              <Link href="/">
-                <Image
-                  className="h-6 w-auto"
-                  width="205"
-                  height="60"
-                  src="/light_logo.png"
-                  alt=""
-                />
-              </Link>
-              <div className="-mr-2 flex items-center lg:hidden">
-                <Popover.Button className="focus-ring-inset relative inline-flex items-center justify-center rounded-md bg-slate-900 bg-opacity-0 p-2 text-black hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-orange-500">
+    <Disclosure as="nav" className="bg-black sticky z-40 top-0">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-orange-700 hover:bg-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+                <div className="flex-shrink-0 flex items-center">
+                  <Image
+                    className="block lg:hidden h-8 w-auto"
+                    src="/dark_logo.png"
+                    height="500"
+                    width="500"
+                    alt="logo"
+                  />
+                  <Image
+                    className="hidden lg:block h-8 w-auto"
+                    src="/dark_logo.png"
+                    height="500"
+                    width="500"
+                    alt="logo"
+                  />
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-blue-900 text-white"
+                            : "text-gray-300 hover:bg-orange-700 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  <Transition
+                    as={Fragment}
+                    enter="transition duration-300 transform"
+                    enterFrom="opacity-0 -translate-y-12"
+                    enterTo="opacity-100 -translate-y-0"
+                    leave="transition duration-300 transform"
+                    leaveFrom="opacity-100 -translate-y-0"
+                    leaveTo="opacity-0 -translate-y-12"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Your Profile
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Settings
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Sign out
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-blue-900 text-white"
+                      : "text-gray-300 hover:bg-orange-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
+
+{
+  /* <Popover.Button className="focus-ring-inset relative inline-flex items-center justify-center rounded-md bg-slate-900 bg-opacity-0 p-2 text-black hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-orange-500">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
-            </div>
-            <div className="hidden space-x-10 lg:ml-10 lg:flex">
-              {navigation.map((item) => (
-                <Link
-                  href={item.href}
-                  key={item.name}
-                  className={classNames(
-                    "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium",
-                    pathname === item.href
-                      ? "border-blue-700 text-black"
-                      : "border-transparent text-black hover:border-orange-500 hover:text-black"
-                  )}
-                  aria-current={pathname === item.href ? "page" : undefined}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </nav>
-
-        <Transition
-          as={Fragment}
-          enter="transition duration-300 transform"
-          enterFrom="opacity-0 -translate-y-12"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition duration-300 transform"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 -translate-y-12"
-        >
-          <Popover.Panel
-            focus
-            className="absolute inset-x-0 top-0 origin-top transform transition lg:hidden"
-          >
-            <div className="overflow-hidden bg-white shadow-lg ring-1 ring-black ring-opacity-5 h-max">
-              <div className="flex items-center justify-between px-5 pt-4">
-                <div>
-                  <Image
-                    className="h-6 w-auto"
-                    src="/light_logo.png"
-                    height="60"
-                    width="205"
-                    alt=""
-                  />
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="relative inline-flex items-center justify-center rounded-md bg-blue-800 p-2 text-white hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-800">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-8 w-8" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="pb-6 pt-5">
-                <div className="space-y-1 px-2">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-orange-500"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
-    </header>
-  );
+                </Popover.Button> */
 }
